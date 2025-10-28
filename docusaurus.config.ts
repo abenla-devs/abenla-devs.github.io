ff939dcf121397a9cbca816a4153dab995ca2b69
tranvietphuoc <phuoctv.ut@gmail.com>
2025-10-28 16:51:50 +0700

    edit docusarus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -61,7 +61,8 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/abenla-devs/abenla-devs.github.io/tree/main/",
+
           // Useful options to enforce blogging best practices
           onInlineTags: "warn",
           onInlineAuthors: "warn",
@@ -95,7 +96,7 @@ const config: Config = {
         },
         { to: "/blog", label: "Blog", position: "left" },
         {
-          href: "https://github.com/facebook/docusaurus",
+          href: "https://github.com/abenla-devs/abenla-devs.github.io",
           label: "GitHub",
           position: "right",
         },
